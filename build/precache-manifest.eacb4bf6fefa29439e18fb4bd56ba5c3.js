2f4c1c7379bb858ea79a45288e42003cde0f4ad1
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 09:15:52 +0300

    Rokas

---
+++ build/precache-manifest.eacb4bf6fefa29439e18fb4bd56ba5c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3536bcbac04299b6ee8bdbd67fae5a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d14d4a188fcf89dbe0",
+    "url": "/static/css/main.8d337989.chunk.css"
+  },
+  {
+    "revision": "f2792414bc26de0a4f66",
+    "url": "/static/js/2.b7caf7ab.chunk.js"
+  },
+  {
+    "revision": "7ed3dd3ec9ec334831b17403bf1ef67f",
+    "url": "/static/js/2.b7caf7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d14d4a188fcf89dbe0",
+    "url": "/static/js/main.a53851df.chunk.js"
+  },
+  {
+    "revision": "d1dd70a8f702026bf8a4",
+    "url": "/static/js/runtime-main.886928d5.js"
+  }
+]);
